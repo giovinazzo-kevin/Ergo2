d8ae1bb5050df1e45c6e92274e5be160873b29c3
Kevin Giovinazzo <kevin.giovinazzo@kappas.it>
2026-06-13 01:41:43 +0200

    tests

--- src/Ergo.Pipelines/OrPipeline.cs
+++ src/Ergo.Pipelines/OrPipeline.cs
@@ -5,9 +5,16 @@ namespace Ergo.Pipelines;
 public sealed class OrPipeline<TInput, TOutput>(
     Pipeline<TInput, TOutput> left,
     Pipeline<TInput, TOutput> right
-) : Pipeline<TInput, TOutput>([], [])
+) : Pipeline<TInput, TOutput>([new OrStep<TInput, TOutput>(left, right)], [default(Unit)!])
 {
-    public override Result<TOutput, PipelineError> Run(TInput input)
+}
+
+internal class OrStep<TInput, TOutput>(
+    Pipeline<TInput, TOutput> left,
+    Pipeline<TInput, TOutput> right
+) : IPipeline<TInput, TOutput, Unit>
+{
+    public Result<TOutput, PipelineError> Run(TInput input, Unit _)
     {
         var result = left.Run(input);
         if (result is Success<TOutput>)
